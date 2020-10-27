973f399bedc0293a8f0bfab0c36192ebf5177ced
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-10-26 22:09:48 -0300

    criacao do row mapper

---
+++ src/main/java/br/com/mjvdevschool/manganews/rowmapper/NoticiaRowMapper.java
@@ -0,0 +1,34 @@
+package br.com.mjvdevschool.manganews.rowmapper;
+
+import br.com.mjvdevschool.manganews.models.Autor;
+import br.com.mjvdevschool.manganews.models.Manga;
+import br.com.mjvdevschool.manganews.models.Noticia;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NoticiaRowMapper implements RowMapper<Noticia> {
+
+    @Override
+    public Noticia mapRow(ResultSet rs, int i) throws SQLException {
+        Noticia noticia = new Noticia();
+
+        noticia.setId(rs.getInt(1));
+        noticia.setTitulo(rs.getString("titulo"));
+        noticia.setCorpo(rs.getString("corpo"));
+        noticia.setDataLançamento(rs.getDate("dataLancamento"));
+        noticia.setVisualizacoes(rs.getInt("visualizacoes"));
+        noticia.setUrlImagem(rs.getString("urlImagem"));
+
+        Autor autor = new Autor();
+
+        autor.setId(rs.getInt(7));
+        autor.setNome(rs.getString("nome"));
+
+        noticia.setAutor(autor);
+
+        return noticia;
+    }
+
+}
