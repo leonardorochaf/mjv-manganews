ab87f331a7305ca6dfcc63bedc84bbf95ed3bbc4
leonardorochaf <leonardo.rocha0803@gmail.com>
2020-10-26 22:08:52 -0300

    criacao do controller

---
+++ src/main/java/br/com/mjvdevschool/manganews/controllers/NoticiaController.java
@@ -0,0 +1,34 @@
+package br.com.mjvdevschool.manganews.controllers;
+
+import br.com.mjvdevschool.manganews.services.NoticiaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class NoticiaController {
+
+    private final NoticiaService noticiaService;
+
+    public NoticiaController(NoticiaService noticiaService) {
+        this.noticiaService = noticiaService;
+    }
+
+    @GetMapping
+    public String listar(ModelMap model) {
+        model.put("noticiasCompletas", noticiaService.buscarTodos());
+        model.put("noticiasMaisVistas", noticiaService.buscarMaisVistos());
+        return "home";
+    }
+
+    @GetMapping("/busca")
+    public String buscar(@RequestParam String parametro, ModelMap model) {
+        System.out.println(parametro);
+        model.put("noticiasResultado", noticiaService.buscarPorParametro(parametro));
+        return "resultadoBusca";
+    }
+
+}
